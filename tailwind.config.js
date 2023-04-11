e88ade9c9a9b669012c05e41f81c58c1318c936f
Vivi Tian <vivi.tian930@gmail.com>
2023-04-11 18:06:35 +1200

    add logo and navbar

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const { color } = require("framer-motion")
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mont: ["var(--font-mont)"]
+      },
+      colors: {
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#B63E96", // 240,86,199
+        primaryDark: "#58E6D9" // 80,230,217
+      }
+    }
+  },
+  plugins: []
+}
