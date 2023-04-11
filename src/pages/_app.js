777127df1c965a00cf14128f25ad8e108c679f86
Vivi Tian <vivi.tian930@gmail.com>
2023-04-11 23:13:57 +1200

    add home page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer"
 import NavBar from "@/components/NavBar"
 import "@/styles/globals.css"
 import { Montserrat } from "next/font/google"
@@ -20,6 +21,7 @@ export default function App({ Component, pageProps }) {
       >
         <NavBar />
         <Component {...pageProps} />
+        <Footer />
       </main>
     </>
   )
