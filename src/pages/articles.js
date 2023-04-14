80e09a0a350e50f37f883bd5199431c7eee7ad88
Vivi Tian <vivi.tian930@gmail.com>
2023-04-15 08:36:16 +1200

    feat: responsive articles page

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -33,7 +33,7 @@ const MovingImage = ({ title, img, link }) => {
       onMouseMove={handleMouse}
       onMouseLeave={handleMouseLeave}
     >
-      <h2 className="capitalize text-xl font-semibold hover:underline">
+      <h2 className="capitalize text-xl font-semibold hover:underline md:text-lg">
         {title}
       </h2>
       <FramerImage
@@ -41,7 +41,7 @@ const MovingImage = ({ title, img, link }) => {
         ref={imgRef}
         src={img}
         alt={title}
-        className="z-10 w-96 h-auto hidden absolute rounded-lg"
+        className="z-10 w-96 h-auto hidden absolute rounded-lg md:!hidden"
       />
     </Link>
   )
@@ -73,7 +73,7 @@ const FeaturedArticle = ({ time, title, summary, img, link }) => {
         />
       </Link>
       <Link href={link} target="_blank">
-        <h2 className="my-2 mt-4 capitalize  text-2xl font-bold hover:underline">
+        <h2 className="my-2 mt-4 capitalize  text-2xl font-bold hover:underline xs:text-lg">
           {title}
         </h2>
       </Link>
@@ -93,10 +93,16 @@ const Article = ({ title, date, img, link }) => {
       viewport={{ once: true }}
       className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between
     bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4
-    dark:border-light dark:bg-dark dark:text-light"
+    dark:border-light dark:bg-dark dark:text-light
+    sm:flex-col
+    "
     >
       <MovingImage title={title} img={img} link={link} />
-      <span className="text-primary font-semibold pl-4 dark:text-primaryDark">
+      <span
+        className="text-primary font-semibold pl-4 dark:text-primaryDark
+      sm:self-start sm:pl-0 xs:text-sm
+      "
+      >
         {date}
       </span>
     </motion.li>
@@ -112,8 +118,11 @@ const articles = () => {
       </Head>
       <main className="w-full flex flex-col items-center justify-center overflow-hidden dark:text-light">
         <Layout className="pt-16">
-          <AnimatedText text="Words Can Change The World!" className="mb-16" />
-          <ul className="grid grid-cols-2 gap-16">
+          <AnimatedText
+            text="Words Can Change The World!"
+            className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl"
+          />
+          <ul className="grid grid-cols-2 gap-16 lg:gap-8 md:grid-cols-1 md:gap-y-16">
             <FeaturedArticle
               title="Build A Custom Pagination Component In Reactjs From Scratch"
               summary="Learn how to build a custom pagination component in ReactJS from scratch. 
