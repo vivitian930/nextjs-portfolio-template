064224ea2d3915bcfeddade71baba863b1d81b37
Vivi Tian <vivi.tian930@gmail.com>
2023-04-14 20:32:58 +1200

    feat: responsive menu to replace navbar for small screens

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -32,6 +32,32 @@ const CustomLink = ({ href, title, className = "" }) => {
   )
 }
 
+const CustomMobileLink = ({ href, title, className = "", toggle }) => {
+  const router = useRouter()
+  const handleClick = () => {
+    toggle()
+    router.push(href)
+  }
+  return (
+    <button
+      href={href}
+      className={`${className} relative group text-light dark:text-dark my-2`}
+      onClick={handleClick}
+    >
+      {title}
+      <span
+        className={`h-[1px] inline-block  dark:bg-dark bg-light 
+        absolute left-0 -bottom-0.5 group-hover:w-full 
+        transition-[width] ease duration-300 ${
+          router.asPath === href ? "w-full" : "w-0"
+        }`}
+      >
+        &nbsp;
+      </span>
+    </button>
+  )
+}
+
 const NavBar = () => {
   const [mode, setMode] = useThemeSwitcher()
   const [isOpen, setIsOpen] = useState(false)
@@ -41,7 +67,7 @@ const NavBar = () => {
   }
 
   return (
-    <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light">
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light relative">
       <button
         className="flex-col justify-center items-center hidden lg:flex"
         onClick={handleClick}
@@ -129,6 +155,101 @@ const NavBar = () => {
           </button>
         </nav>
       </div>
+
+      {isOpen ? (
+        <div
+          className="flex min-w-[70vw] flex-col justify-between items-center
+      z-30 bg-dark/90 dark:bg-light/90 rounded-lg backdrop-blue-md py-32 fixed top-1/2 left-1/2
+       -translate-x-1/2 -translate-y-1/2"
+        >
+          <nav className="flex items-center flex-col justify-center">
+            <CustomMobileLink
+              href="/"
+              title="Home"
+              className=""
+              toggle={handleClick}
+            />
+            <CustomMobileLink
+              href="/about"
+              title="About"
+              className=""
+              toggle={handleClick}
+            />
+            <CustomMobileLink
+              href="/projects"
+              title="Projects"
+              className=""
+              toggle={handleClick}
+            />
+            <CustomMobileLink
+              href="/articles"
+              title="Article"
+              className=""
+              toggle={handleClick}
+            />
+          </nav>
+          {/* <h2>haha</h2> */}
+          <nav className="flex items-center justify-center flex-wrap mt-2">
+            <motion.a
+              href="https://twitter.com"
+              target={"_blank"}
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mx-3 sm:mx-1"
+            >
+              <TwitterIcon />
+            </motion.a>
+            <motion.a
+              href="https://twitter.com"
+              target={"_blank"}
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mx-3  bg-light rounded-full dark:bg-dark sm:mx-1"
+            >
+              <GithubIcon />
+            </motion.a>
+            <motion.a
+              href="https://twitter.com"
+              target={"_blank"}
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mx-3 sm:mx-1"
+            >
+              <LinkedInIcon />
+            </motion.a>
+            <motion.a
+              href="https://twitter.com"
+              target={"_blank"}
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mx-3 bg-light rounded-full sm:mx-1"
+            >
+              <PinterestIcon />
+            </motion.a>
+            <motion.a
+              href="https://twitter.com"
+              target={"_blank"}
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 ml-3 sm:mx-1"
+            >
+              <DribbbleIcon />
+            </motion.a>
+            <button
+              onClick={() => setMode(mode === "light" ? "dark" : "light")}
+              className={`ml-3 flex items-center justify-center rounded-full p-1 sm:mx-1 ${
+                mode === "light" ? "bg-dark text-light" : "bg-light text-dark"
+              }`}
+            >
+              {mode === "dark" ? (
+                <SunIcon className={"fill-dark"} />
+              ) : (
+                <MoonIcon className={"fill-dark"} />
+              )}
+            </button>
+          </nav>
+        </div>
+      ) : null}
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
         <Logo />
       </div>
